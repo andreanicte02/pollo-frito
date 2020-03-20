681a5ec179946d0b45662b9d09fcb432e67aec26
Andrea <andreanicte02@gmail.com>
2020-03-20 16:48:09 -0600

    asf

--- src/com/polloenpelotas/GUI2.java
+++ src/com/polloenpelotas/GUI2.java
@@ -3,14 +3,14 @@ package com.polloenpelotas;
 import javax.swing.*;
 
 public class GUI2 extends JFrame {
-    private JTextArea textArea1;
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
-    private JButton button1;
-    private JButton button2;
+    private JTextArea textArea1;
     private JButton button3;
+    private JButton abrirButton;
+    private JButton button2;
     private JButton button4;
-    private JButton button5;
+    private JButton addPestaña;
     private JButton button6;
     private JButton button7;
     private JButton button8;
@@ -24,4 +24,13 @@ public class GUI2 extends JFrame {
     private JButton button16;
     private JButton button18;
     private JButton button19;
+
+    public GUI2(){
+        super("Interprete");
+        this.setContentPane(panel1);
+
+        this.textArea1.setSize(100,100);
+        this.setSize(700,700);
+
+    }
 }
