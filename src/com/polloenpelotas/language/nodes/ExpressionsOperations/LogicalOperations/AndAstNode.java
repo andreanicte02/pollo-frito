0cf02828d18c88e740651d920b1a0f20b10376d3
Andrea <andreanicte02@gmail.com>
2020-04-01 03:27:14 -0600

    corto circuito

--- src/com/polloenpelotas/language/nodes/ExpressionsOperations/LogicalOperations/AndAstNode.java
+++ src/com/polloenpelotas/language/nodes/ExpressionsOperations/LogicalOperations/AndAstNode.java
@@ -1,11 +1,34 @@
 package com.polloenpelotas.language.nodes.ExpressionsOperations.LogicalOperations;
 
+import com.polloenpelotas.language.ChickenUtils;
 import com.polloenpelotas.language.FileLocation;
+import com.polloenpelotas.language.LocatedSemanticException;
+import com.polloenpelotas.language.SemanticException;
 import com.polloenpelotas.language.nodes.AstNode;
 import com.polloenpelotas.language.nodes.BinaryOperationNode;
+import com.polloenpelotas.language.types.ZBoolean;
+import com.polloenpelotas.language.types.ZProtoObject;
+import org.jetbrains.annotations.NotNull;
 
 public class AndAstNode extends BinaryOperationNode {
     public AndAstNode(FileLocation fl, AstNode e1, AstNode e2) {
         super(fl, "and", "&", e1, true, e2);
+    }
+
+
+    @Override
+    public ZProtoObject safeExecute(@NotNull ZProtoObject ambit) throws LocatedSemanticException, SemanticException {
+
+        if(!ChickenUtils.valueCond("Condicion en and",e1,ambit)){
+            return new ZBoolean(false);
+        }
+
+        if(!ChickenUtils.valueCond("Condicion en and",e2,ambit)){
+            return new ZBoolean(false);
+        }
+
+
+       return new ZBoolean(true);
+
     }
 }
--- src/com/polloenpelotas/language/nodes/ExpressionsOperations/LogicalOperations/OrAstNode.java
+++ src/com/polloenpelotas/language/nodes/ExpressionsOperations/LogicalOperations/OrAstNode.java
@@ -1,11 +1,32 @@
 package com.polloenpelotas.language.nodes.ExpressionsOperations.LogicalOperations;
 
+import com.polloenpelotas.language.ChickenUtils;
 import com.polloenpelotas.language.FileLocation;
+import com.polloenpelotas.language.LocatedSemanticException;
+import com.polloenpelotas.language.SemanticException;
 import com.polloenpelotas.language.nodes.AstNode;
 import com.polloenpelotas.language.nodes.BinaryOperationNode;
+import com.polloenpelotas.language.types.ZBoolean;
+import com.polloenpelotas.language.types.ZProtoObject;
+import org.jetbrains.annotations.NotNull;
 
 public class OrAstNode extends BinaryOperationNode {
     public OrAstNode(FileLocation fl, AstNode e1, AstNode e2) {
         super(fl, "or", "|", e1, true, e2);
+    }
+
+    public ZProtoObject safeExecute(@NotNull ZProtoObject ambit) throws LocatedSemanticException, SemanticException {
+
+        if(ChickenUtils.valueCond("Condicion en and",e1,ambit)){
+            return new ZBoolean(true);
+        }
+
+        if(ChickenUtils.valueCond("Condicion en and",e2,ambit)){
+            return new ZBoolean(true);
+        }
+
+
+        return new ZBoolean(false);
+
     }
 }
